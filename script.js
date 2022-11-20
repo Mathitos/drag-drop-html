4f63b779419deecee5c5a2dc3c7db6c06233d6e9
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2022-11-20 16:13:01 -0300

    add drag animation

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log('hi')
+
+const cards = document.querySelectorAll('.card')
+const lists = document.querySelectorAll('.list')
+
+cards.forEach((card) => {
+  card.addEventListener('dragstart', () => {
+    card.classList.add('moving')
+  })
+
+  card.addEventListener('dragend', () => {
+    card.classList.remove('moving')
+  })
+})
